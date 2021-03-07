b4fa961b4ead25856fcb97e9cd4adf4f52a6af89
ermolaevio <ermolaevio@sovcombank.ru>
2021-03-07 17:01:23 +0300

    implemented logic for SearchWordViewModel.kt to search meanings and method to open meaning screen

--- app/src/main/java/com/ermolaevio/search4meanings/AppRouter.kt
+++ app/src/main/java/com/ermolaevio/search4meanings/AppRouter.kt
@@ -1,4 +1,8 @@
 package com.ermolaevio.search4meanings
 
 class AppRouter {
+
+    fun openMeaning(id: String) {
+
+    }
 }
--- app/src/main/java/com/ermolaevio/search4meanings/viewModel/SearchWordViewModel.kt
+++ app/src/main/java/com/ermolaevio/search4meanings/viewModel/SearchWordViewModel.kt
@@ -6,26 +6,62 @@ import androidx.lifecycle.ViewModelProvider
 import com.ermolaevio.search4meanings.AppRouter
 import com.ermolaevio.search4meanings.domain.interactor.SearchMeaningInteractor
 import com.ermolaevio.search4meanings.domain.models.FoundWord
+import io.reactivex.Scheduler
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.subjects.PublishSubject
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 
 class SearchWordViewModel(
     private val interactor: SearchMeaningInteractor,
-    private val router: AppRouter
+    private val router: AppRouter,
+    private val schedulers: Scheduler
 ) : ViewModel() {
 
     class SearchWordViewModelFactory @Inject constructor(
         private val interactor: SearchMeaningInteractor,
-        private val router: AppRouter
+        private val router: AppRouter,
+        private val schedulers: Scheduler
     ) : ViewModelProvider.Factory {
 
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            return SearchWordViewModel(interactor, router) as T
+            return SearchWordViewModel(interactor, router, schedulers) as T
         }
     }
 
     val result = MutableLiveData<List<FoundWord>>(emptyList())
     val loading = MutableLiveData<Boolean>(false)
+    private val searchSubject = PublishSubject.create<String>()
+    private val disposables = CompositeDisposable()
 
+    init {
+        searchSubject.debounce(300, TimeUnit.MILLISECONDS)
+            .filter { it.isNotBlank() }
+            .distinctUntilChanged()
+            .doOnNext { loading.postValue(true) }
+            .switchMapSingle { interactor.search(it) }
+            .observeOn(schedulers)
+            .subscribe({
+                loading.value = false
+                result.value = it
+            }, {
+                loading.value = false
+                result.value = emptyList()
+            })
+            .addTo(disposables)
+    }
 
+    fun search(text: String) {
+        searchSubject.onNext(text)
+    }
 
+    fun openMeaning(id: String) {
+        router.openMeaning(id)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.clear()
+    }
 }
